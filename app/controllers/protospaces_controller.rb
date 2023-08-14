17e133908f4dc0046795885d0b46eabd1af08765
Aya-Nori <atc.btm@gmail.com>
2023-08-14 11:58:16 +0900

    ログインとログアウト実装

--- app/controllers/protospaces_controller.rb
+++ app/controllers/protospaces_controller.rb
@@ -1,4 +1,16 @@
 class ProtospacesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+
   def index
   end
+
+  def show
+    @name = current_user.name
+  end
+  
+  def destroy
+    redirect_to root_path
+  end
+
 end
