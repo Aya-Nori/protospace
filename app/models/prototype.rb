c2cde29bac80bdf26b1268d4b8fc85009d3b6235
Aya-Nori <atc.btm@gmail.com>
2023-08-14 15:25:56 +0900

    バリデーション設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,12 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
