8cf9996015842a46907d6d00e166384f5c3b43a9
Aya-Nori <atc.btm@gmail.com>
2023-08-21 23:19:25 +0900

    詳細ページで投稿の情報表示

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,7 +4,7 @@ class Prototype < ApplicationRecord
   
   validates :title, presence: true
   validates :catch_copy, presence: true
-  validates :concept, presence: true
+  validates :concept, presence: true, unless: :was_attached?
 
   def was_attached?
     self.image.attached?
