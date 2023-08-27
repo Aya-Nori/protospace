23816503abe32da3a78a0976394bec975f208a8b
Aya-Nori <atc.btm@gmail.com>
2023-08-27 14:31:27 +0900

    投稿したコメントが詳細ページで表示される

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,6 +6,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def new
