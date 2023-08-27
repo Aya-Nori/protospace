417ed0e04f1a3546edfde6e2a1be80f616d7a3de
Aya-Nori <atc.btm@gmail.com>
2023-08-27 14:08:16 +0900

    コメント投稿機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
-    redirect_to "/prototypes/#{comment.prototype.id}"
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
+    end
   end
 
   private
